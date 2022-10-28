eaff21cc06e70bb48f8c6264fbcf1edf7e7b3e83
h4ckit <dammysteve10@gmail.com>
2022-10-28 23:32:12 +0100

    [change] Minor Change

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	y := yanker.NewYankManager("https://21.download.real-debrid.com/d/IEPVL5VCVXL5Q/She-Hulk.Attorney.at.Law.S01E09.1080p.HEVC.x265-MeGusta%5Beztv.re%5D.mkv")
-
+	y := yanker.NewYankManager("https://55.download.real-debrid.com/d/SBDLRYUYBWBY6/She-Hulk.Attorney.at.Law.S01E04.720p.WEB.x265-MiNX.mkv", yanker.Options{ConcurrentConnections: 10})
+	//y := yanker.NewYankManager("https://www.seedr.cc/download/archive/ef65f774dc8f06171c970d9221bf05a24f597c1211a690974706c079534f6657?token=1fc5b0daf95382c09050ac3149483c31406ddb155886b8916efc28a7fdad91e9&exp=1667052850")
 	y.StartDownload()
 
 }
